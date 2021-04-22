1ba9aac687b989369ff98a33d8ccc05aec77b1dd
AlexJohnson2 <salambarf@gmail.com>
2021-04-22 23:51:51 +0430

    created user model and added to admin, and makemigration,migrate and createsuperuser, and edit gitignore from standard gitignore for django

--- sarayemaryam/web/models.py
+++ sarayemaryam/web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255, default="")
+    password = models.CharField(max_length=255, default="")
+    phone = models.CharField(max_length=255, default="")
+    address = models.CharField(max_length=255, default="")
+    post_code = models.CharField(max_length=255, default="")

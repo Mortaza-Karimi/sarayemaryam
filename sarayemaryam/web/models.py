b50d1cf867303ca7ac7ac50c6b5d19383f8734d1
AlexJohnson2 <salambarf@gmail.com>
2021-05-05 00:34:06 +0430

    add urls and create register view

--- sarayemaryam/web/models.py
+++ sarayemaryam/web/models.py
@@ -8,3 +8,7 @@ class User(models.Model):
     phone = models.CharField(max_length=255, default="")
     address = models.CharField(max_length=255, default="")
     post_code = models.CharField(max_length=255, default="")
+    def __str__(self):
+        unserlines = "__________________________________________________________________________"
+        underlines2 = (len(unserlines) - len(self.username)) * "_"
+        return self.username + underlines2 + self.phone
--- sarayemaryam/web/views.py
+++ sarayemaryam/web/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from .models import *
 
 # Create your views here.
+
+@require_http_methods(['POST'])
+@csrf_exempt
+def register_user(request):
+    context = {}
+
+    if request.POST.get('username')\
+    and request.POST.get('password')\
+    and request.POST.get('phone'):
+        context['status'] = 'ok'
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        phone = request.POST.get('phone')
+        User.objects.create(username=username, password=password, phone=phone)
+        context['status'] = 'ok'
+    return JsonResponse(context, encoder=JSONEncoder)

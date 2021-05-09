157fbe16ca4024ed1d5ace5ea9e01260e899cb3b
AlexJohnson2 <salambarf@gmail.com>
2021-05-09 23:18:12 +0430

    finished register view and created login view

--- sarayemaryam/web/models.py
+++ sarayemaryam/web/models.py
@@ -9,6 +9,4 @@ class User(models.Model):
     address = models.CharField(max_length=255, default="")
     post_code = models.CharField(max_length=255, default="")
     def __str__(self):
-        unserlines = "__________________________________________________________________________"
-        underlines2 = (len(unserlines) - len(self.username)) * "_"
-        return self.username + underlines2 + self.phone
+        return self.username + "_______________" + self.phone
--- sarayemaryam/web/views.py
+++ sarayemaryam/web/views.py
@@ -10,15 +10,57 @@ from .models import *
 @require_http_methods(['POST'])
 @csrf_exempt
 def register_user(request):
+    """ this function for register user with API
+    """
     context = {}
 
     if request.POST.get('username')\
     and request.POST.get('password')\
     and request.POST.get('phone'):
-        context['status'] = 'ok'
+
         username = request.POST.get('username')
         password = request.POST.get('password')
         phone = request.POST.get('phone')
-        User.objects.create(username=username, password=password, phone=phone)
-        context['status'] = 'ok'
+
+        users_list = User.objects.filter(username = username)
+        phones_list = User.objects.filter(phone = phone)
+
+        if users_list:
+            context['status'] = 'error'
+            context['message'] = 'Duplicate username'
+        else:
+            if phones_list:
+                context['status'] = 'error'
+                context['message'] = 'Duplicate phone'
+            else:
+                User.bojects.create(username=username, password=password, phone=phone)
+
+                context['status'] = 'ok'
+                context['username'] = username
+                context['password'] = password
+                context['phone'] = phone
     return JsonResponse(context, encoder=JSONEncoder)
+
+
+@require_http_methods(['POST'])
+@csrf_exempt
+def login(request):
+    """ this function for check username with password and return user info
+    """
+
+    context = {}
+
+    if request.POST.get('username')\
+    and request.POST.get('password'):
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        users_list = User.objects.filter(username = username, password = password)
+
+        if users_list:
+            user = User.objects.filter(username = username, password = password).get()
+            context['status'] = 'ok'
+        else:
+            context['status'] = 'error'
+            context['meessage'] = 'username or password is wrong'
